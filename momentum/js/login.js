e4c3ae4f26482e80acd4ae2e66d685753ab044b8
Hyunjung Lee <leeleehj77@gmail.com>
2022-04-19 16:36:44 -0400

    Momentum App Project

---
+++ momentum/js/login.js
@@ -0,0 +1,40 @@
+const loginForm = document.getElementById("login-form");
+const loginInput = document.querySelector("#login-form input");
+// const loginInput = loginForm.querySelector("input");
+
+function onLoginBtnClick() {
+    const username = loginInput.value;
+    console.log(username);
+}
+
+loginForm.addEventListener("submit", onLoginBtnClick);
+
+
+/*
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
+*/
