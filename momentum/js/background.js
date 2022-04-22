dc9c499d2cd59b8fc167babcfd57b7a0f11f0f4c
Hyunjung Lee <leeleehj77@gmail.com>
2022-04-21 21:50:55 -0400

    Random Background Image in Momentum App

--- momentum/js/background.js
+++ momentum/js/background.js
@@ -1,8 +1,10 @@
 const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-// createElement()
+// createElement() - javaScript에서 html element 만들기
 const bgImage = document.createElement("img");
 bgImage.src = `img/${chosenImage}`;
 // body에 추가
-document.body.appendChild(bgImage);
+document.body.appendChild(bgImage);ㄴ
+// append - 가장 뒤에 추가
+// prepend - 가장 앞에 추가
