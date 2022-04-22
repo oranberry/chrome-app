221272dee5109c5df1243194f777705efc8eb7b8
Hyunjung Lee <leeleehj77@gmail.com>
2022-04-22 15:11:31 -0400

    Random Background Images in Momentum App

--- momentum/js/background.js
+++ momentum/js/background.js
@@ -1,4 +1,4 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const images = ["0.jpeg", "1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg", "6.jpeg", "7.jpeg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 // createElement() - javaScript에서 html element 만들기
